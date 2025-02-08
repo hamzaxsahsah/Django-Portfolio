04be44f9524f9c118fd79eb9f260e2804e1483e3
Hamza Sahsah <nanobig2000@gmail.com>
2025-02-08 16:26:51 +0100

    [ADD] last stuff

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ python manage.py migrate
 # Set superuser credentials securely
 export DJANGO_SUPERUSER_EMAIL="hamza@admin.com"
 export DJANGO_SUPERUSER_USERNAME="admin"
-export DJANGO_SUPERUSER_PASSWORD ="Grandfucker1945"
+export DJANGO_SUPERUSER_PASSWORD="Grandfucker1945"
 # Create the superuser
 python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL" --username "$DJANGO_SUPERUSER_USERNAME" --password "$DJANGO_SUPERUSER_PASSWORD" || echo "Superuser already exists."
 
