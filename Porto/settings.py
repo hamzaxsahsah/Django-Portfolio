576a6505eb17868c56fd02339bf50f1776f9721f
Hamza Sahsah <nanobig2000@gmail.com>
2025-02-08 15:06:19 +0100

    [ADD] last stuff

--- Porto/settings.py
+++ Porto/settings.py
@@ -121,17 +121,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-if (
-    not DEBUG
-):  # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Ensure STATIC_ROOT is always set
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]  # Optional: If you have additional static folders
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+if not DEBUG:
+    # Use WhiteNoise for static file handling in production
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# Media files
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
